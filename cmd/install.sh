a7b9e7c994c7dd74220b636442f081aa4c8a48da
Stepan Ananin <xrf848@gmail.com>
2024-12-19 23:02:36 +0300

    installation script fixed
    installation script improved
    
    hotfix

--- cmd/install.sh
+++ cmd/install.sh
@@ -1,5 +1,51 @@
+#!/bin/bash
+
+HELP="Available installation options:
+        \n\t '--local' - install in /usr/local/bin
+        \n\t '--user' - install in /usr/bin
+        \n\t '--global' - install in /bin"
+
+if [ ! "$#" == 1 ]; then
+    echo "Invalid amount of parameters. Only one parameter required - installation option"
+    echo -e $HELP
+    exit 1
+fi
+
+opts=("--local" "--user" "--global")
+
+if [[ ! " ${opts[*]} " =~ " $1 " ]]; then
+    echo "Invalid installation option: ${1}"
+    echo -e $HELP
+    exit 1
+fi
+
 go build main.go
 
-mv main pwgen
+if [[ $? -ne 0 ]]; then
+    echo "Failed to build pwgen"
+    exit 1
+fi
 
-mv aboba /bin
+chmod +x main
+
+if [ $1 == "--local" ]; then
+    sudo mv main /usr/local/bin/pwgen
+fi
+
+if [ $1 == "--user" ]; then
+    sudo mv main /usr/bin/pwgen
+fi
+
+if [ $1 == "--global" ]; then
+    sudo mv main /bin/pwgen
+fi
+
+if [[ -e "main" ]]; then
+    echo "clean up"
+    rm main
+fi
+
+if [[ ! $? -ne 0 ]]; then
+    echo "pwgen installed"
+fi
+
